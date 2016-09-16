67b37bb67535c96967ebdd00a984ac1ffb1080a8
greatpie <greatpie@live.com>
2016-09-17 00:12:21 +0800

    fix:增加产品页面样式

--- dev/app/components/production.js
+++ dev/app/components/production.js
@@ -1,14 +1,14 @@
 /**
  * Created by pie on 2016-9-16.
  */
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import {Card} from 'antd';
 import ProductionConfig from 'data/ProductionConfig';
 
 export default class Production extends Component {
     render() {
-        const cardBody = ProductionConfig.Card.data.map((item,index)=>
-            <Card title={item.title} key={index}>
+        const cardBody = ProductionConfig.Card.data.map((item, index)=>
+            <Card className="custom-card" title={item.title} key={index}>
                 <p>{item.type}</p>
                 <p>{item.interest}</p>
                 <p>{item.desc}</p>
@@ -16,8 +16,10 @@ export default class Production extends Component {
         );
 
         return (
-            <div className="home-wrapper">
-                {cardBody}
+            <div className="production-wrapper">
+                <div className="section-container">
+                    {cardBody}
+                </div>
             </div>
         );
     }
